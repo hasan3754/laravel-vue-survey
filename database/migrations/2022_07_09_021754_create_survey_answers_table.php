f40e8f801b246d2b5786feb9f8ed88630a961bd0
mirazhasan47 <mirazlaradev@gmail.com>
2022-07-09 17:00:39 +0600

    frontend done

---
+++ database/migrations/2022_07_09_021754_create_survey_answers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSurveyAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Survey::class, 'survey_id');
+            $table->timestamp('start_date')->default(\DB::raw('CURRENT_TIMESTAMP'));;
+            $table->timestamp('end_date')->default(\DB::raw('CURRENT_TIMESTAMP'));;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey_answers');
+    }
+}
