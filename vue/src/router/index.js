014673a15c137b4529e50472ae213d870c7ca918
mirazhasan47 <mirazlaradev@gmail.com>
2022-07-11 13:06:39 +0600

    survey start

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from '../views/Dashboard.vue'
 import Surveys from '../views/Surveys.vue'
 import Login from "../views/Login.vue"
+import SurveyView from "../views/SurveyView.vue"
 import Register from "../views/Register.vue"
 import DefaultLayout from "../components/DefaultLayout.vue"
 import AuthLayout from "../components/AuthLayout.vue"
@@ -16,7 +17,9 @@ const routes = [
         meta: {requiresAuth: true},
         children: [
             { path: '/dashboard', name: 'Dashboard', component: Dashboard },
-            { path: '/surveys', name: 'Surveys', component: Surveys }
+            { path: '/surveys', name: 'Surveys', component: Surveys },
+            { path: '/surveys/create', name: 'SurveyCreate', component: SurveyView },
+            { path: '/surveys/:id', name: 'SurveyView', component: SurveyView }
         ]
     },
     {
